e59c2de53732143b864831d6afbff4088babc6b9
John Lillis <JohnMichaelFFS@gmail.com>
2013-01-26 15:13:40 -0500

    Created the basic models

--- ourstorybook/storyviewer/models.py
+++ ourstorybook/storyviewer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Story model (represents an entire story, made up of Chapters)
+class Story(models.Model):
+    title = models.CharField(max_length=140)
+    author = models.ForeignKey(User)
+
+# Chapter model (represents a chapter/branch of a Story)
+class Chapter(models.Model):
+    title = models.CharField(max_length=140)
+    author = models.ForeignKey(User)
+    story = models.ForeignKey(Story)
+    parent = models.ForeignKey('Chapter')
+    content = models.TextField()
