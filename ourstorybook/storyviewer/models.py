76ffb69db7f19a2864e1a511f3a896022e0b9e46
John Lillis <JohnMichaelFFS@gmail.com>
2013-01-26 15:56:23 -0500

    Added __unicode__ methods to models

--- ourstorybook/storyviewer/models.py
+++ ourstorybook/storyviewer/models.py
@@ -3,11 +3,14 @@ from django.contrib.auth.models import User
 
 # Story model (represents an entire story, made up of Chapters)
 class Story(models.Model):
-    class Meta():
-        verbose_name_plural = 'stories'
-
     title = models.CharField(max_length=140)
     author = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta():
+        verbose_name_plural = 'stories'
 
 # Chapter model (represents a chapter/branch of a Story)
 class Chapter(models.Model):
@@ -16,3 +19,6 @@ class Chapter(models.Model):
     story = models.ForeignKey(Story)
     parent = models.ForeignKey('Chapter')
     content = models.TextField()
+
+    def __unicode__(self):
+        return '%s: %s'% (self.story.title, self.title)
