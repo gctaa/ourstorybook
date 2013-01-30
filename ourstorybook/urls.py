b2a3d23cdbf961de5ca63c998f9b2e4f68467693
John Lillis <JohnMichaelFFS@gmail.com>
2013-01-29 20:25:35 -0500

    Added base and story_list templates, added to urls.py

--- ourstorybook/urls.py
+++ ourstorybook/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import ListView, RedirectView
+from storyviewer.models import Story
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ourstorybook.views.home', name='home'),
-    # url(r'^ourstorybook/', include('ourstorybook.foo.urls')),
-
+    url(r'^$', RedirectView.as_view(url="/stories/")),
+    url(r'^stories/$', ListView.as_view(model=Story, queryset=Story.objects.all())),
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
