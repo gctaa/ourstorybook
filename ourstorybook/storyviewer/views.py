33f5f4937c7891126a9e009d7544469642ec5fa1
John Lillis <JohnMichaelFFS@gmail.com>
2013-02-16 16:13:08 -0500

    Added Chapter creation form

--- ourstorybook/storyviewer/views.py
+++ ourstorybook/storyviewer/views.py
@@ -2,12 +2,13 @@ from django.views.generic import CreateView
 from django.http import HttpResponseRedirect
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import permission_required
-from forms import StoryCreateForm
+from forms import StoryCreateForm, ChapterCreateForm
+from models import Story
 
 class StoryCreationView(CreateView):
     form_class = StoryCreateForm
 
-    @method_decorator(permission_required("storyviewer.edit_story"))
+    @method_decorator(permission_required('storyviewer.edit_story'))
     def dispatch(self, *args, **kwargs):
         return super(StoryCreationView, self).dispatch(*args, **kwargs)
 
@@ -15,4 +16,24 @@ class StoryCreationView(CreateView):
         object = form.save(commit=False)
         object.author = self.request.user
         object.save()
-        return HttpResponseRedirect("/stories/")
+        return HttpResponseRedirect('/stories/')
+        
+class ChapterCreationView(CreateView):
+    form_class = ChapterCreateForm
+
+    @method_decorator(permission_required('storyviewer.edit_chapter'))
+    def dispatch(self, *args, **kwargs):
+        self.story = Story.objects.get(id=kwargs['pk'])
+        return super(ChapterCreationView, self).dispatch(*args, **kwargs)
+        
+    def get_context_data(self, **kwargs):
+        context = super(ChapterCreationView, self).get_context_data(**kwargs)
+        context['story'] = self.story
+        return context
+
+    def form_valid(self, form):
+        object = form.save(commit=False)
+        object.author = self.request.user
+        object.story = self.story
+        object.save()
+        return HttpResponseRedirect('/stories/%s/' % self.story.id) # TODO
