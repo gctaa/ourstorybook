7963055d1b8063a5080f566258450ed1180c3f2e
John Lillis <JohnMichaelFFS@gmail.com>
2013-02-19 19:28:03 -0500

    Added extra security setting
    In response to the Django update:
    https://www.djangoproject.com/weblog/2013/feb/19/security/

--- ourstorybook/settings_heroku.py
+++ ourstorybook/settings_heroku.py
@@ -2,6 +2,10 @@ from base_settings import *
 import os
 import dj_database_url
 
+ALLOWED_HOSTS = (
+    '.herokuapp.com',
+)
+
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
