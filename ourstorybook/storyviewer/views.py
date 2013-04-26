0ae047c8429ff2646b62c0141d3251903d3c60b7
John Lillis <JohnMichaelFFS@gmail.com>
2013-04-25 20:40:25 -0400

    Added ability to create pages via the site

--- ourstorybook/storyviewer/views.py
+++ ourstorybook/storyviewer/views.py
@@ -12,6 +12,8 @@ class StoryCreationView(CreateView):
 
     @method_decorator(permission_required('storyviewer.edit_story'))
     def dispatch(self, *args, **kwargs):
+        self.request.session['last_story_id'] = None
+        self.request.session['last_page_id'] = None
         return super(StoryCreationView, self).dispatch(*args, **kwargs)
 
     def form_valid(self, form):
@@ -24,6 +26,11 @@ class StoryDetailView(DetailView):
     model = Story
     template_name = 'storyviewer/story_detail.html'
     
+    def dispatch(self, *args, **kwargs):
+        self.request.session['last_story_id'] = self.get_object().id
+        self.request.session['last_page_id'] = None
+        return super(StoryDetailView, self).dispatch(*args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super(StoryDetailView, self).get_context_data(**kwargs)
         context['nodes'] = Page.objects.filter(story=self.object)
@@ -34,27 +41,17 @@ class PageCreationView(CreateView):
     template_name = 'storyviewer/page_create.html'
 
     #
-    #   TODO - attach storyid and parentid to POST request in forms/template
-    #   ensure input is cleaned
+    #   TODO - ensure input is cleaned
     #   do some magic in form_valid to ensure parents are set (see django-mptt docs)
     #
     @method_decorator(permission_required('storyviewer.edit_page'))
     def dispatch(self, *args, **kwargs):
-        # On GET requests, story/parent_page ID will come in via the url querystrings
-        # On POST requests, the story/parent_page ID will come in via POST data
-        if self.request.method == 'GET':
-            request_method = self.request.GET
-        elif self.request.method == 'POST':
-            request_method = self.request.POST
-        
-        #import pdb; pdb.set_trace();
-        
-        self.story = get_object_or_404(Story, pk=request_method.get('story'))
-        try:
-            self.parent_page = Page.objects.get(pk=request_method.get('parent_page'))
-        except Page.DoesNotExist:
+        self.story = get_object_or_404(Story, pk=self.request.session.get('last_story_id'))
+        parent_page_id = self.request.session.get('last_page_id')
+        if parent_page_id:
+            self.parent_page = Page.objects.get(pk=parent_page_id)
+        else:
             self.parent_page = None
-        
         return super(PageCreationView, self).dispatch(*args, **kwargs)
         
     def get_context_data(self, **kwargs):
@@ -65,8 +62,9 @@ class PageCreationView(CreateView):
 
     def form_valid(self, form):
         object = form.save(commit=False)
-        object.author = self.request.user
         object.story = self.story
+        object.parent = self.parent_page
+        object.author = self.request.user
         object.save()
         return HttpResponseRedirect('/stories/%s/' % self.story.id)
         
@@ -74,6 +72,11 @@ class PageDetailView(DetailView):
     model = Page
     template_name = 'storyviewer/page_detail.html'
     
+    def dispatch(self, *args, **kwargs):
+        self.request.session['last_story_id'] = self.get_object().story.id
+        self.request.session['last_page_id'] = self.get_object().id
+        return super(PageDetailView, self).dispatch(*args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super(PageDetailView, self).get_context_data(**kwargs)
         context['nodes'] = self.object.get_descendants()
